2ee62cff645d073e080e60aa91eda7c006381e06
Shannon Young <shannon.ari1@gmail.com>
2020-12-20 17:28:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,20 +1,27 @@
 def my_hash
-    my_hash = { "a" => "b"}
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"Name" => "Shannon Young", "Age" => "37", "Status" => "Single"}
 end
 
 
 def shipping_manifest
-    hash = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  #set a variable called `shipping_manifest`, equal to a hash
+  #fill that hash with key/value pairs that describe the following information: 
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  
   shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 
 end
+
